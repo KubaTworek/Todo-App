d653635d6551a1f641ab21a01dc613849cdf3de7
KubaTworek <kubatvorek@gmail.com>
2022-08-10 22:25:22 +0200

    Initial commit

---
+++ src/main/java/pl/jakubtworek/springdemo/entity/Todo.java
@@ -0,0 +1,54 @@
+package pl.jakubtworek.springdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="todo")
+public class Todo {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="text")
+	private String text;
+	
+	public Todo() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer{" +
+				"id=" + id +
+				", text='" + text + '\'' +
+				'}';
+	}
+}
+
+
+
+
+
